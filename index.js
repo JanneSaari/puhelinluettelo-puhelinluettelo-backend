5fe3f30ebc4a2ab46d959fbeb2155e7dc9e245ef
Janne Saari <jannesaari24@gmail.com>
2023-08-08 16:21:37 +0300

    Finished 3.3

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
 const express = require('express')
 const app = express()
 
-let phonenumbers = []
+let phonenumbers = [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+    },
+    {
+      "name": "Test Name",
+      "number": "39-23-6423122",
+      "id": 5
+    }
+  ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+  app.get('/info', (req, res) => {
+    const count = phonenumbers.length
+    res.send(`<p>Phonebook has info for ${count} people</p>`)
   })
   
-  app.get('/api/phonenumbers', (req, res) => {
-    res.json(notes)
+  app.get('/api/persons', (req, res) => {
+    res.json(phonenumbers)
+  })
+
+  app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const number = phonenumbers.find(number => number.id === id)
+    
+    if (number) {
+      res.json(number)
+    } else {
+      res.status(404).end()
+    }
   })
   
   const PORT = 3001
