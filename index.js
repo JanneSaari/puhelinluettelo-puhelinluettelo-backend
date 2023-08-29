c03710492979f22864d089143e2d80282eb4da57
Janne Saari <jannesaari24@gmail.com>
2023-08-29 15:46:29 +0300

    Finish 3.16

--- index.js
+++ index.js
@@ -27,75 +27,57 @@ app.use(morgan((tokens, req, res) => {
   ].join(' ')
 }))
 
-let phonenumbers = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    }
-  ]
-
-  app.get('/info', (req, res) => {
+  app.get('/info', (req, res, next) => {
     Phonenumber.find({})
     .then(numbers => {
       res.send(`<p>Phonebook has info for ${numbers.length} people</p>`)
     })
     .catch(() => {
       console.log('Failed to get phonenumbers')
+      next(error)
     })
   })
   
-  app.get('/api/persons', (req, res) => {
+  app.get('/api/persons', (req, res, next) => {
     Phonenumber.find({})
     .then(numbers => {
       res.json(numbers)
     })
-    .catch(() => {
+    .catch(error => {
       console.log('Failed to get phonenumbers')
+      next(error)
     })
   })
 
-  app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-
-    Phonenumber.find({id: id})
+  app.get('/api/persons/:id', (req, res, next) => {
+    Phonenumber.findById(req.params.id)
     .then(number => {
       console.log(number)
+      // res.json(number)
       if (number) {
         res.json(number)
       } else {
         res.status(404).end()
       }
     })
-    .catch(() => {
-      console.log('Failed to get phonenumbers')
+    .catch(error => {
+      next(error)
     })
   })
 
-  app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    numbers = phonenumbers.filter(number => number.id !== id)
-    phonenumbers = numbers
-  
-    res.status(204).end()
+  app.delete('/api/persons/:id', (req, res, next) => {
+    // const id = Number(req.params.id)
+    // numbers = phonenumbers.filter(number => number.id !== id)
+    // phonenumbers = numbers
+
+    Phonenumber.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
   })
 
-  app.post('/api/persons', (req, res) => {
+  app.post('/api/persons', (req, res, next) => {
     const body = req.body
     console.log('Adding to phonebook:', body)
 
@@ -126,14 +108,31 @@ let phonenumbers = [
       console.log(result)
       res.json(person)
     })
+    .catch(error => next(error))
   })
-
-  const generateID = () => {
-    const maxID = 1000000
-    return Math.floor((Math.random() * maxID))
-  }
   
   const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
+
+
+  //#### Error handling #####
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  // olemattomien osoitteiden käsittely
+  app.use(unknownEndpoint)
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+  }
+  // virheellisten pyyntöjen käsittely
+  app.use(errorHandler)
